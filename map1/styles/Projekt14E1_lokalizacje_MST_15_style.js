70e0987eb9de6bd74f71bed572dea38046ef97b5
MACIEJ\macie <maciekszypulski@gmail.com>
2024-01-17 21:27:25 +0100

    14E1 project updated to new spec.

---
+++ map1/styles/Projekt14E1_lokalizacje_MST_15_style.js
@@ -0,0 +1,42 @@
+var size = 0;
+var placement = 'point';
+
+var style_Projekt14E1_lokalizacje_MST_15 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    var clusteredFeatures = feature.get("features");
+    var labelFont = "10.4px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    size = clusteredFeatures.length;
+    var textAlign = "center";
+    var offsetX = 0;
+    var offsetY = 0;
+    if (size == 1) {
+        textAlign = "left"
+        offsetX = 8
+        offsetY = 3
+        var feature = clusteredFeatures[0];
+        if ("" !== null) {
+            labelText = String("");
+        }
+        key = value + "_" + labelText
+    } else {
+        labelText = size.toString()
+        size = 2*(Math.log(size)/ Math.log(2))
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 5.6 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
---
+++ map1/styles/wrakstatku_8_style.js
@@ -0,0 +1,42 @@
+var size = 0;
+var placement = 'point';
+
+var style_wrakstatku_8 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    var clusteredFeatures = feature.get("features");
+    var labelFont = "7.800000000000001px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 0.5;
+    size = clusteredFeatures.length;
+    var textAlign = "center";
+    var offsetX = 0;
+    var offsetY = 0;
+    if (size == 1) {
+        textAlign = "left"
+        offsetX = 8
+        offsetY = 3
+        var feature = clusteredFeatures[0];
+        if (feature.get("short_name") !== null) {
+            labelText = String(feature.get("short_name"));
+        }
+        key = value + "_" + labelText
+    } else {
+        labelText = size.toString()
+        size = 2*(Math.log(size)/ Math.log(2))
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 2.8 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(227,26,28,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
